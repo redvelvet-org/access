f837007d2182b620894c8c47fad54b9ad502ba9a
Anoop Varghese <anupvarghese@gmail.com>
2017-05-30 07:49:43 +1000

    fix middleware

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -3,11 +3,10 @@ const logger = require('../lib/logger');
 const errors = (err, req, res, next) => {
   if (err.isBoom) {
     const { payload } = err.output;
-    res.boom.create(parseInt(payload.statusCode, 10), payload.error);
-    next();
+    next(res.status(parseInt(payload.statusCode, 10)).json({ error: payload.error }));
   } else {
     logger.error(err);
-    next(res.boom.create(500, 'Unable to process'));
+    next(res.status(500).json({ error: 'Unable to process' }));
   }
 };
 

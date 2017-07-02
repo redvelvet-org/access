86ffc3f46d2bcff7ff9067f82d0e9dea64f73f40
Anoop Varghese <anupvarghese@gmail.com>
2017-07-02 13:13:12 +1000

    new migrations

---
+++ migrations/20170702094600-add-user.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      hash: {
+        type: Sequelize.STRING,
+        unique: false,
+        allowNull: false,
+        defaultValue: ''
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    }).then(() => queryInterface.addIndex('users', ['email'], { indicesType: 'UNIQUE' }));
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('users');
+  }
+};
